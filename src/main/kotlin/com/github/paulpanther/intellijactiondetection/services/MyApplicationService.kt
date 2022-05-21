f0d715cd4edd425b42365fdc3876db3302ad50bc
GitHub Action <action@github.com>
2022-05-21 09:27:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/paulpanther/intellijactiondetection/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.paulpanther.intellijactiondetection.services
+
+import com.github.paulpanther.intellijactiondetection.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/paulpanther/intellijactiondetection/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paulpanther/intellijactiondetection/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paulpanther.intellijactiondetection.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paulpanther.intellijactiondetection.MyBundle
 
 class MyProjectService(project: Project) {
 
