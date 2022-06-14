a0e8f1221739d18a69369b6c39551e73f41f1f98
Antonius Naumann <git@antonius.dev>
2022-06-14 16:59:49 +0200

    Implement timeline graph

--- src/main/kotlin/com/paulpanther/actiondetector/actions/Timeline.kt
+++ src/main/kotlin/com/paulpanther/actiondetector/actions/Timeline.kt
@@ -3,12 +3,72 @@ package com.paulpanther.actiondetector.actions
 import com.github.gumtreediff.actions.model.Action
 
 class Timeline {
+    // Edges are oriented from oldest to newest snapshot
+    private val graph: MutableMap<Snapshot, Edges> = mutableMapOf()
+    private var first: Snapshot? = null
+    private var last: Snapshot? = null
 
     fun add(newSnap: Snapshot, newDiff: Map<Snapshot, List<Action>>) {
-        TODO()
+        graph[newSnap] = Edges(
+            incoming = newDiff.map { (snapshot, actions) ->
+                TimelineEdge(origin = snapshot, destination = newSnap, actions).also { graph.getValue(snapshot).outgoing.add(it) }
+            }.toMutableList(),
+            outgoing = mutableListOf())
+
+        first = first ?: newSnap
+        last = newSnap
     }
 
     fun findShortestPath(): List<Action> {
-        TODO()
+        val first: Snapshot = this.first ?: return listOf()
+        val last: Snapshot = this.last ?: return listOf()
+
+        val queue = mutableListOf(graph.getValue(first))
+        val visited = graph.mapValues { (_, _) -> 0 }.toMutableMap()
+        val distances = graph.mapValues { (_, _) -> Int.MAX_VALUE }.toMutableMap()
+        val shortest = mutableMapOf<Snapshot, TimelineEdge>()
+        distances[first] = 0
+
+        while(queue.isNotEmpty()) {
+            val edges = queue.removeAt(0)
+
+            edges.outgoing.forEach { edge ->
+                visited[edge.destination] = visited.getValue(edge.destination) + 1
+                val dist = edge.weight + (distances.getValue(edge.origin))
+                if (dist < distances.getValue(edge.destination)) {
+                    distances[edge.destination] = dist
+                    shortest[edge.destination] = edge
+                }
+
+                if (visited.getValue(edge.destination) == graph.getValue(edge.destination).incoming.size) {
+                    queue.add(graph.getValue(edge.destination))
+                }
+            }
+        }
+
+        var current = last
+        val path = mutableListOf<TimelineEdge>()
+        while (current != first) {
+            val edge = shortest.getValue(current)
+            path += edge
+            current = edge.origin
+        }
+
+        return path
+            .reversed()
+            .flatMap { it.actions }
     }
+}
+
+
+private data class Edges(
+    val incoming: MutableList<TimelineEdge>,
+    val outgoing: MutableList<TimelineEdge>
+)
+private data class TimelineEdge(
+    val origin: Snapshot,
+    val destination: Snapshot,
+    val actions: List<Action>
+) {
+    val weight = actions.size
 }
