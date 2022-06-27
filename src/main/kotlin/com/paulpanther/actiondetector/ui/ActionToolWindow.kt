b8a8b060a851bb6f4865d6f30e690c0a8928a63f
Paul Methfessel <paul.methfessel@web.de>
2022-06-27 11:06:35 +0200

    Show changed code in tool window

--- src/main/kotlin/com/paulpanther/actiondetector/ui/ActionToolWindow.kt
+++ src/main/kotlin/com/paulpanther/actiondetector/ui/ActionToolWindow.kt
@@ -1,11 +1,20 @@
 package com.paulpanther.actiondetector.ui
 
 import com.github.gumtreediff.actions.model.Action
+import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.project.Project
+import com.intellij.psi.JavaCodeFragmentFactory
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.ui.EditorTextField
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.components.fields.ExpandableTextField
+import com.intellij.ui.dsl.builder.Panel
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.layout.cellPanel
 import com.paulpanther.actiondetector.actionService
+import com.paulpanther.actiondetector.actions.DisplayAction
 import com.paulpanther.actiondetector.actions.displayName
+import java.awt.BorderLayout
 import java.awt.event.ActionEvent
 import javax.swing.JButton
 import javax.swing.JPanel
@@ -38,20 +47,50 @@ class ActionToolWindow(private val project: Project) {
     }
 
     fun createUIComponents() {
-        contentPanel = JPanel()
-        actionsPanel = JPanel()
-        contentPanel?.add(JBScrollPane(actionsPanel))
+        contentPanel = JPanel().apply {
+            layout = BorderLayout()
+        }
+        actionsPanel = JPanel().apply {
+            layout = BorderLayout()
+        }
+        contentPanel?.add(JBScrollPane(actionsPanel), BorderLayout.CENTER)
     }
 
     @Suppress("UnstableApiUsage")
-    private fun buildEntries(refactorings: List<Action>) {
+    private fun buildEntries(actions: List<Action>) {
         actionsPanel?.add(panel {
-            for (ref in refactorings) {
+            @Suppress("UnstableApiUsage")
+            for (action in actions) {
                 row {
-                    label(ref.displayName)
+                    if (action is DisplayAction) {
+                        panel {
+                            collapsibleGroup(action.title) {
+                                buildDetails(this, action)
+                            }
+                        }
+                    } else {
+                        label(action.displayName)
+                    }
                 }
             }
-        })
+        }, BorderLayout.NORTH)
     }
 
+    @Suppress("UnstableApiUsage")
+    private fun buildDetails(p: Panel, action: DisplayAction) {
+        for (detail in action.details) {
+            p.row {
+                detail.label?.let {
+                    label(it)
+                }
+                label(detail.code)
+            }
+        }
+    }
+
+    private fun highlightCode(code: String): JPanel {
+        val fragment = JavaCodeFragmentFactory.getInstance(project).createExpressionCodeFragment(code, null, null, true)
+        val doc = PsiDocumentManager.getInstance(project).getDocument(fragment)
+        return EditorTextField(doc, project, JavaFileType.INSTANCE)
+    }
 }
